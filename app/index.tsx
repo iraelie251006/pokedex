9eb7287de20018de934f24f7777f7eff6d171334
Elie <niyubwayoiraelie5777@gmail.com>
2025-11-16 16:41:14 +0200

    enhance Pokémon display with type colors and additional details

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,41 @@
 import { useEffect, useState } from "react";
-import { Image, ScrollView, Text, View } from "react-native";
+import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
 
 interface Pokemon {
   name: string,
   image: string,
   imageBack: string,
+  types: PokemonType[]
 }
+
+interface PokemonType {
+  type: {
+    name: string,
+    url: string,
+  }
+}
+
+const colorsByType: Record<string, string> = {
+  normal:  "#A8A77A",
+  fire:    "#EE8130",
+  water:   "#6390F0",
+  electric:"#F7D02C",
+  grass:   "#7AC74C",
+  ice:     "#96D9D6",
+  fighting:"#C22E28",
+  poison:  "#A33EA1",
+  ground:  "#E2BF65",
+  flying:  "#A98FF3",
+  psychic: "#F95587",
+  bug:     "#A6B91A",
+  rock:    "#B6A136",
+  ghost:   "#735797",
+  dragon:  "#6F35FC",
+  dark:    "#705746",
+  steel:   "#B7B7CE",
+  fairy:   "#D685AD",
+};
+
 export default function Index() {
   const [pokemons, setPokemons] = useState<Pokemon[]>([])
   useEffect(() => {
@@ -27,6 +57,7 @@ export default function Index() {
             name: pokemon.name,
             image: details.sprites.front_default,
             imageBack: details.sprites.back_default,
+            types: details.types
           }
         }
       ))
@@ -38,20 +69,50 @@ export default function Index() {
     }
   }
   return (
-    <ScrollView>
-      {pokemons.map(({name, image, imageBack}) => (
-        <View key={name}>
-          <Text>{name}</Text>
-          <Image
+    <ScrollView
+      contentContainerStyle={{
+        gap: 16,
+        padding: 16
+      }}
+    >
+      {pokemons.map(({name, image, imageBack, types}) => (
+        // @ts-ignore
+        <View 
+          key={name} 
+          style={{
+            backgroundColor: colorsByType[types[0].type.name] + 60,
+            padding: 20,
+            borderRadius: 20,
+          }}>
+          <Text style={styles.name}>{name}</Text>
+          <Text style={styles.type}>{types[0].type.name}</Text>
+          <View style={{flexDirection: "row"}}>
+            <Image
              source={{uri: image}}
              style={{width: 150, height: 150}}
-          />
-          <Image
-             source={{uri: imageBack}}
-             style={{width: 150, height: 150}}
-          />
+            />
+            <Image
+              source={{uri: imageBack}}
+              style={{width: 150, height: 150}}
+            />
+          </View>
+          
         </View>
       ))}
     </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  name: {
+    fontSize: 28,
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  type: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "gray",
+    textAlign: "center"
+  }
+})
